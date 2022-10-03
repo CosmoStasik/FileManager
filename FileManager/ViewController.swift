c8ca2b1beb9635b0a29d2a40f8ab55d8526f0ca3
Stanislav Sobolevski <minuel.colt@gmail.com>
2022-10-03 14:29:20 +0300

    add Project

--- FileManager/ViewController.swift
+++ FileManager/ViewController.swift
@@ -6,14 +6,73 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
+class Person: Codable {
+    var name: String
+    var surname: String
+    var pet: Pet?
+    internal init(name: String, surname: String, pet: Pet?) {
+        self.name = name
+        self.surname = surname
+        self.pet = pet
+    }
+}
+class Pet: Codable {
+    var name: String
+    var ownersName: String
+    internal init(name: String, ownersName: String) {
+        self.name = name
+        self.ownersName = ownersName
+    }
+}
+class ViewController: UIViewController, FileManagerDelegate {
+    let person = Person(name: "Vasya", surname: "Cosmo", pet: nil)
+    let pet = Pet(name: "Simba", ownersName: "Vasya")
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+        let fileManager = FileManager()
+        fileManager.delegate = self
+        person.pet = pet
+        let encoder = JSONEncoder()
+        let decoder = JSONDecoder()
+        var preferenceUrl = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask)[0]
+            do {
+                let data = try encoder.encode(person)
+                preferenceUrl.append(component: "person")
+                try data.write(to: preferenceUrl)
+                } catch {
+                print(error)
+                }
+                do {
+                    let person1 = try decoder.decode(
+                        Person.self,
+                        from: FileManager.default.contents(atPath: preferenceUrl.path) ?? .init())
+                    print(person1.name)
+                } catch {
+                    print(error)
+                }
+        writeSomeData(data: "myImage")
+        writeSomeData(data: "myPhoto")
+        writeSomeData(data: "myData")
+        writeSomeData(data: "myFile")
+        writeSomeData(data: "myDocument")
+            }
+            func writeSomeData(data: String) {
+                var cacheUrl = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask)[0]
+                var documentUrl = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+                cacheUrl.append(component: data)
+                do {
+                    try data.write(to: cacheUrl, atomically: true, encoding: .utf8)
+                    if FileManager.default.fileExists(atPath: documentUrl.path() + "/\(data)")
+                        && cacheUrl.lastPathComponent != "person"{
+                        try FileManager.default.removeItem(atPath: documentUrl.path() + "/\(data)")
+                    }
+                    if cacheUrl.lastPathComponent != "myImage" {
+                        documentUrl.append(component: data)
+                        try FileManager.default.moveItem(at: cacheUrl, to: documentUrl)
+                    }
+                } catch {
+                    print(error)
+                }
+            }
 }
-
---
+++ Podfile
@@ -0,0 +1,8 @@
+target 'FileManager' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+ 
+pod 'SwiftLint'
+
+end
